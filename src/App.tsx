3f52ee02e187b7db750e2c6f9bf0b7727d7b4773
Mycah DeTorres <mycahdetorres@wifi-roaming-128-4-94-193.wifi.udel.edu>
2022-03-18 17:43:38 -0400

    finished sketch

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
@@ -16,6 +16,9 @@ import { EditMode } from "./form-components/EditMode";
 import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
 import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
+import { Button } from "react-bootstrap";
+import { ShowHideTasks } from "./ShowHideTasks";
+import { Sketch } from "./quizzer/Sketch";
 
 function App(): JSX.Element {
     return (
@@ -25,38 +28,21 @@ function App(): JSX.Element {
             </header>
             <Quizzer></Quizzer>
             <hr></hr>
-            <CheckAnswer expectedAnswer="42"></CheckAnswer>
+            <ShowHideTasks></ShowHideTasks>
             <hr></hr>
-            <GiveAttempts></GiveAttempts>
+            <div>
+                <img
+                    src={require("./mySketch.jpg")}
+                    alt="Image of my sketch."
+                />
+            </div>
             <hr></hr>
-            <EditMode></EditMode>
-            <hr></hr>
-            <ChangeColor></ChangeColor>
-            <hr></hr>
-            <MultipleChoiceQuestion
-                options={["a", "b", "c"]}
-                expectedAnswer="b"
-            ></MultipleChoiceQuestion>
-            <hr></hr>
-            <DoubleHalf></DoubleHalf>
-            <hr></hr>
-            <ChooseTeam></ChooseTeam>
-            <hr></hr>
-            <ColoredBox></ColoredBox>
-            <hr></hr>
-            <ShoveBox></ShoveBox>
-            <hr></hr>
-            <Counter></Counter>
-            <hr />
-            <RevealAnswer></RevealAnswer>
-            <hr />
-            <StartAttempt></StartAttempt>
-            <hr />
-            <TwoDice></TwoDice>
-            <hr />
-            <ChangeType></ChangeType>
-            <hr />
-            <CycleHoliday></CycleHoliday>
+            <div>
+                Completed Features:
+                <ul>
+                    <li>Application is Sketched</li>
+                </ul>
+            </div>
         </div>
     );
 }
