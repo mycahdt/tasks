c52f58528cc834e231cfd20e3293349e11366f81
Mycah DeTorres <mycahdetorres@wifi-roaming-128-4-94-193.wifi.udel.edu>
2022-03-18 17:50:36 -0400

    added sketch, fixed App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
-import { ChangeType } from "./components/ChangeType";
-import { RevealAnswer } from "./components/RevealAnswer";
-import { StartAttempt } from "./components/StartAttempt";
-import { TwoDice } from "./components/TwoDice";
-import { CycleHoliday } from "./components/CycleHoliday";
-import { Counter } from "./components/Counter";
-import { DoubleHalf } from "./bad-components/DoubleHalf";
-import { ColoredBox } from "./bad-components/ColoredBox";
-import { ShoveBox } from "./bad-components/ShoveBox";
-import { ChooseTeam } from "./bad-components/ChooseTeam";
-import { CheckAnswer } from "./form-components/CheckAnswer";
-import { GiveAttempts } from "./form-components/GiveAttempts";
-import { EditMode } from "./form-components/EditMode";
-import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
-import { ChangeColor } from "./form-components/ChangeColor";
+//import { ChangeType } from "./components/ChangeType";
+//import { RevealAnswer } from "./components/RevealAnswer";
+//import { StartAttempt } from "./components/StartAttempt";
+//import { TwoDice } from "./components/TwoDice";
+//import { CycleHoliday } from "./components/CycleHoliday";
+//import { Counter } from "./components/Counter";
+//import { DoubleHalf } from "./bad-components/DoubleHalf";
+//import { ColoredBox } from "./bad-components/ColoredBox";
+//import { ShoveBox } from "./bad-components/ShoveBox";
+//import { ChooseTeam } from "./bad-components/ChooseTeam";
+//import { CheckAnswer } from "./form-components/CheckAnswer";
+//import { GiveAttempts } from "./form-components/GiveAttempts";
+//import { EditMode } from "./form-components/EditMode";
+//import { MultipleChoiceQuestion } from "./form-components/MultipleChoiceQuestion";
+//import { ChangeColor } from "./form-components/ChangeColor";
 import { Quizzer } from "./quizzer/Quizzer";
-import { Button } from "react-bootstrap";
+//import { Button } from "react-bootstrap";
 import { ShowHideTasks } from "./ShowHideTasks";
-import { Sketch } from "./quizzer/Sketch";
+//import { Sketch } from "./quizzer/Sketch";
 
 function App(): JSX.Element {
     return (
